95312ef84c6a0a364b8d4cbd17052ff2ad8e24ec
DDarrow123 <ddarrowphl@gmail.com>
2018-11-09 16:39:07 -0500

    first backend commit

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Note.create(title: "exercise", content: "take a class")
+Note.create(title: "pet kitties", content: "pet kitties a lot")
+Note.create(title: "cook", content: "make pasta")
+Note.create(title: "fiance time", content: "get a drink")
+Note.create(title: "clean", content: "dust all surfaces")
+Note.create(title: "go shopping", content: "buy winter wardrobe")
